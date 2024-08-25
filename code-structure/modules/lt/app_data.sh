806c10067232c730f3a7525370f7659cc330e1a2
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-26 00:36:36 +0530

    ansible command running on the frontned and backend machines

--- code-structure/modules/lt/app_data.sh
+++ code-structure/modules/lt/app_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dnf install python3.12-pip.noarch -y | tee -a /opt/ouput.log
-pip3.12 install botocore boto3 -y | tee -a /opt/output.log
+pip3.12 install botocore boto3 | tee -a /opt/output.log
 dnf install ansible -y | tee -a /opt/output.log
 ansible-pull -i localhost, -U https://github.com/sriharsha6197/expense-ansible.git -e ansible_user=centos -e ansible_password=DevOps321 expense.yaml -e role_name=${component} | tee -a /opt/output.log
