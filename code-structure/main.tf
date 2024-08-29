6e0b4f996b171fc1f2d22b28d89e1466fbe936f4
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-29 19:57:59 +0530

    referencing tg group arn value in the module lt

--- code-structure/main.tf
+++ code-structure/main.tf
@@ -38,7 +38,7 @@ module "lt" {
   private_subnets = module.vpc.PRIVATE_SUBNETS
   terraform_controller_instance = var.terraform_controller_instance
   app_port = each.value == "frontend" ? var.frontend_app_port : var.backend_app_port
-  target_group = module.lt.aws_lb_target_group.arn
+  target_group = module.lt.aws_lb_target_group.value
 }
 
 module "rds" {
