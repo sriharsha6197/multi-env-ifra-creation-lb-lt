f814cf806558b74db4b5a2ca588a463087be1b6b
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-09-01 23:03:12 +0530

    scdv

--- code-structure/modules/lt/outputs.tf
+++ code-structure/modules/lt/outputs.tf
@@ -1,6 +1,6 @@
 output "aws_lb_tg" {
   value = {
-    for tg in var.alb_type_internal : tg.name => lookup(aws_lb_target_group.tg[*].name,"arn","Not Found")
+    for tg in var.alb_type_internal : tg.name => lookup(zipmap(range(length(aws_lb_target_group.tg[*].name)),aws_lb_target_group.tg[*].name),"arn","Not Found")
   }
 }
 
